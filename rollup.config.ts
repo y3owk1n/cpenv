82472f5730036cc2b173c8219186e103359ab3a4
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-12-01 18:22:55 +0800

    feat: add action to test the build (#39)
    * fix: add release id to gh action
    
    * chore: add test action
    
    * fix: add root dir to typescript conf
    
    * feat: improve rollup
    
    * fix: tsconfig and try again
    
    * fix: try to see if it works
    
    * fix: another try
    
    * fix: justfile to use js instead

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,13 +1,14 @@
+import { defineConfig } from "rollup";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 
-export default {
+export default defineConfig({
 	input: "src/index.ts",
 	output: {
-		file: "dist/index.cjs",
+		dir: "dist",
 		format: "cjs",
 	},
 	plugins: [
@@ -16,8 +17,10 @@ export default {
 		resolve(), // Helps Rollup find external modules
 		typescript({
 			tsconfig: "./tsconfig.json", // Ensure this matches your TypeScript configuration
-			noForceEmit: true,
+			rootDir: "./src",
+			declaration: true,
+			outDir: "./dist/types",
 		}),
 		terser(), // Minification
 	],
-};
+});
