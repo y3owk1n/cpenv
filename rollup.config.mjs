e29bbfce56cdc8160034cf5f988e1572495b0a3a
y3owk1n <wongyeowkin@gmail.com>
2024-12-01 13:04:46 +0800

    feat: move to rollup instead of tsup

---
+++ rollup.config.mjs
@@ -0,0 +1,23 @@
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+
+export default {
+	input: "src/index.ts",
+	output: {
+		file: "dist/index.cjs",
+		format: "cjs",
+	},
+	plugins: [
+		json(),
+		commonjs(),
+		resolve(), // Helps Rollup find external modules
+		typescript({
+			tsconfig: "./tsconfig.json", // Ensure this matches your TypeScript configuration
+			noForceEmit: true,
+		}),
+		terser(), // Minification
+	],
+};
